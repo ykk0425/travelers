68042e1b1c701dea0397056b7709392bd19dbebb
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-04-19 11:56:12 +0000

    [Add]Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheet/application";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
