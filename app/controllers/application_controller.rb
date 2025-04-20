b16b9e981474a7d76ade5cf3cf284aec78951e28
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-04-20 08:42:31 +0000

    [Add]ログアウト後遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
