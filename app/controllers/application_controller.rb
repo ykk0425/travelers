b9d209f8b1656cf876ead2c69da29db1c842c426
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-04-20 12:10:11 +0000

    [AdUpdate]viewファイル

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
 end
