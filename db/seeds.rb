3a8699523123ab9ac425995426ef99560ed68a51
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-05-03 06:30:22 +0000

    [Add]Usseedファイル User情報

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#ユーザ情報
+taro = User.find_or_create_by!(email: "taro@example.com") do |user|
+  user.name = "旅好太郎"
+  user.password = "tabitaro"
+  user.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename: "sample-user1.jpg")
+end
+
+mama = User.find_or_create_by!(email: "mama@example.com") do |user|
+  user.name = "旅行好きママ"
+  user.password = "tabimama"
+  user.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename: "sample-user2.jpg")
+end
+
+couple = User.find_or_create_by!(email: "couple@example.com") do |user|
+  user.name = "旅するカップル"
+  user.password = "tabicouple"
+  user.profile_image.attach(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename: "sample-user3.jpg")
+end
+
