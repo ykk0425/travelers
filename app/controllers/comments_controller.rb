656cf20174347bd231030cee42da023eed65979d
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-05-08 08:46:29 +0000

    [Update]comments_controller内記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    plan = Plan.find(params[:plan_id])
+    comment = current_user.comments.new(comment_params)
+    comment.plan_id = plan.id
+    comment.save
+    redirect_to plan_path(plan)
+  end
+  
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+
 end
