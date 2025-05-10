68e9b7a337c982c449aa03d2bfa85b6e155f8fa1
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-05-10 01:57:55 +0000

    [Add]admin/model,controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users, controllers: { registrations: 'users/registrations' }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
