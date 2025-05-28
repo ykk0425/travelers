67fbbf3b6d741b59461a22b9de95c27cad8a52ae
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-05-28 07:44:08 +0000

    [Add]スポット住所入力viewファイル

--- app/controllers/public/plans_controller.rb
+++ app/controllers/public/plans_controller.rb
@@ -72,7 +72,7 @@ class Public::PlansController < ApplicationController
     # planパラメータ
     def plan_params
       params.require(:plan).permit(:user_id, :title, :review, :start_date, :end_date, :body,
-        spots_attributes: [:id, :name, :latitude, :longitude, :visit_order, :staying_start, :staying_end, :explanation, :_destroy, {images: []},
+        spots_attributes: [:id, :name, :latitude, :address, :longitude, :visit_order, :staying_start, :staying_end, :explanation, :_destroy, {images: []},
           routes_attributes: [:id, :departure, :arrival, :travel_time, :transportation, :description, :visit_order, :_destroy
           ]
         ])
