041e51f1787676bc35ded69ba8113dbea1fbac64
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-06-18 07:54:10 +0000

    [Update]Remove jpostal.js

--- app/javascript/packs/jpostal.js
+++
@@ -1,9 +0,0 @@
-function jpostal() {
-  $('#zipcode').jpostal({
-    postcode : ['#zipcode'],
-    address : {
-      '#spot_address': '%3%4%5'
-    }
-  });
-}
-$(document).on("turbolinks:load", jpostal);
