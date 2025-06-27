6ee5a75858e795497efb49b3a83e89268ba77a84
ykk0425 <187173741+ykk0425@users.noreply.github.com>
2025-06-27 06:13:57 +0000

    [Update]]ゲストユーザー名

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "guestuser"
+      user.name = "ゲスト"
     end
   end
 
